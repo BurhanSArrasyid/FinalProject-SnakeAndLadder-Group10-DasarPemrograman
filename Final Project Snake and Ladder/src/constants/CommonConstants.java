0e90310b38cde2a144f5bdb91277b26054fac499
BurhanSArrasyid <162649285+BurhanSArrasyid@users.noreply.github.com>
2024-06-28 22:25:45 +0700

    CommonConstants.java

--- Final Project Snake and Ladder/src/constants/CommonConstants.java
+++ Final Project Snake and Ladder/src/constants/CommonConstants.java
@@ -3,9 +3,9 @@ package constants;
 import java.awt.*;
 
 public class CommonConstants {
-    public static final Color PRIMARY_COLOR = Color.decode("#191E29");
-    public static final Color SECONDARY_COLOR = Color.decode("#132D46");
-    public static final Color TEXT_COLOR = Color.decode("#01C38D");
+    public static final Color PRIMARY_COLOR = Color.decode("#EAE2B7");
+    public static final Color SECONDARY_COLOR = Color.decode("#DDA15E");
+    public static final Color TEXT_COLOR = Color.decode("#6B705C");
 
     public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/loginschema";
     public static final String DB_USERNAME = "root";
