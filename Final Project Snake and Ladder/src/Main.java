9c9db0eae8e3e6ca547288e781313694954dacaf
BurhanSArrasyid <162649285+BurhanSArrasyid@users.noreply.github.com>
2024-07-01 21:40:56 +0700

    Main.java

--- Final Project Snake and Ladder/src/Main.java
+++ Final Project Snake and Ladder/src/Main.java
@@ -1,3 +1,18 @@
+/**
+ * -----------------------------------------------------
+ * ES234211 - Programming Fundamental
+ * Genap - 2023/2024
+ * Group Capstone Project: Snake and Ladder Game
+ * -----------------------------------------------------
+ * Class    : D
+ * Group    : 10
+ * Members  :
+ * 1. 5026231074 - Burhan Shidqi Arrasyid
+ * 2. 5026231106 - Nailah Qonitah Firdausa
+ * 3. 5026231231 - Sinta Dewi Rahmawati
+ * ------------------------------------------------------
+ */
+
 import db.MyJDBC;
 import guis.LoginFormGUI;
 import guis.RegisterFormGUI;
